2713c4e24d6a8513bf5321d18847b70aed4bf035
AustinT33 <austintumlinson@gmail.com>
2019-12-02 16:25:37 -0600

    worked on drill 3

--- app.js
+++ app.js
@@ -24,6 +24,26 @@ app.get('/cipher', (req,res) => {
     res.send(`'the cipher is: ${message.join('')}'`)
 })
 
+
+app.get('/lotto', (req, res)=> {
+    const number = req.query.n;
+    const num = Number(number)
+    const randomNum = Math.floor(Math.random()*7);
+    const lottoNum = [];
+    lottoNum.push(num)
+
+    console.log(randomNum, lottoNum, number)
+
+   for(let i=0; i< 6; i++){
+        if(lottoNum.includes(randomNum[i])){
+        res.send(`'the number ${randomNum} matches ${lottoNum[0]}!`)
+    } else {
+        res.send('no matches');
+    }
+}
+
+})
+
 app.listen(8000, () => {
 console.log('listening')
 });
