14663027d5dc3de69d23f8b2b91ac8012fe9952d
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-12-02 16:35:45 -0500

    cipher drill

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ app.get('/sum', (req, res)=> {
     res.send(`'the sum of ${a} and ${b} is ${c}'`);
 });
 
+app.get('/cipher', (req,res) => {
+    const text = req.query.text;
+    const shift = req.query.shift;
+
+    let code = text.split('').map(char => char.toUpperCase().charCodeAt(0))
+    let cipher = code.map(num => num - shift)
+    console.log(cipher)
+    let message = cipher.map(num => String.fromCharCode(num))
+    
+
+    res.send(`'the cipher is: ${message.join('')}'`)
+})
+
 app.listen(8000, () => {
 console.log('listening')
 });
